ef7338dfc98cc8a7ef8d2f377bcd3643323ba8a2
Christopher Seil <christopher.seil@ssi-schaefer.com>
2023-08-29 13:52:09 +0200

    import module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HousesComponent } from './houses/houses.component';
@@ -10,7 +11,8 @@ import { HousesComponent } from './houses/houses.component';
     HousesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
