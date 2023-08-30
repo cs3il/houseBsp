f79c28017818fadb5a5169dd2fa20fa881b08d0b
Christopher Seil <christopher.seil@ssi-schaefer.com>
2023-08-30 13:23:38 +0200

    show house details when house on dashboard is selected

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HousesComponent } from './houses/houses.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HouseDetailComponent } from './house-detail/house-detail.component';
 
 const routes: Routes = [
   {path: 'houses', component: HousesComponent},
   {path: 'dashboard', component: DashboardComponent},
+  {path: 'detail/:id', component: HouseDetailComponent},
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
 ]
 
